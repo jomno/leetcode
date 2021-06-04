4e63bcdbb99fd5cfd7a8291d364131ab0d998be6
wonno <jake89577@gmail.com>
2021-06-04 23:57:18 +0900

    code 정리

--- solutions/1.two_sum/success.rb
+++ solutions/1.two_sum/success.rb
@@ -3,17 +3,19 @@
 # @return {Integer[]}
 
 # 희재 샤라웃 감사감사
+# 반복문이 속도 저하를 일으킨다. 반복문을 줄이기 => 시간 복잡도
 def two_sum(nums, target)
-  ary = []
+  store_ary = []
 
   nums.each_with_index do |num, i|
+    puts i
     other_num = target - num
-    other_num_index = ary.index(other_num)
+    other_num_index = store_ary.index(other_num)
     if other_num_index.nil?
-      ary << num
+      store_ary << num
       next
     else
-      return [i, other_num_index]
+      return [other_num_index, i]
     end
   end
 end
