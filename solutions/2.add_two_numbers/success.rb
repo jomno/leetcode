88e78da293aeeec2a78869fabaa7943ed8b9d9b4
wonno <jake89577@gmail.com>
2021-06-09 00:13:00 +0900

    2021.06.08

--- solutions/2.add_two_numbers/success.rb
+++ solutions/2.add_two_numbers/success.rb
@@ -1,4 +1,7 @@
 # Definition for singly-linked list.
+
+# TODO: 링크드 리스트 개념 복습하기
+# 재귀 코드를 썼는데.. 0 분기 치는 부분이 마음에 들지 않는다. 좀 더 생각해봐야함.
 class ListNode
   attr_accessor :val, :next
 
@@ -20,23 +23,25 @@ class ListNode
     return result
   end
 
-  def self.num_to_list_node(num)
-    return ListNode.new(num % 10, nil) if (num == 0)
-    return nil if (num > 0 && num < 1)
+  def self.num_to_node(num)
+    return ListNode.new(0, nil) if num == 0
 
-    node = ListNode.new((num.floor) % 10, num_to_list_node(num.fdiv(10)))
+    return num_to_recur_node(num)
+  end
+
+  def self.num_to_recur_node(num)
+    return nil if (num <= 0)
+
+    node = ListNode.new(num % 10, num_to_recur_node(num / 10))
     return node
   end
 end
-
-# list_node = ListNode.num_to_list_node(179)
-# puts list_node.find_node_num
 
 # @param {ListNode} l1
 # @param {ListNode} l2
 # @return {ListNode}
 def add_two_numbers(l1, l2)
   sum = l1.find_node_num + l2.find_node_num
-  
-  return ListNode.num_to_list_node(sum)
+
+  return ListNode.num_to_node(sum)
 end
--- solutions/2.add_two_numbers/try_2.rb
+++ solutions/2.add_two_numbers/try_2.rb
@@ -20,20 +20,26 @@ class ListNode
     return result
   end
 
-  def self.num_to_list_node(num)
-    return ListNode.new(num % 10, nil) if (num == 0)
-    return nil if (num > 0 && num < 1)
+  def self.num_to_node(num)
+    return ListNode.new(0, nil) if num == 0
 
-    node = ListNode.new((num.floor) % 10, num_to_list_node(num.fdiv(10)))
+    return num_to_recur_node(num)
+  end
+
+  def self.num_to_recur_node(num)
+    return nil if (num <= 0)
+
+    node = ListNode.new(num % 10, num_to_recur_node(num / 10))
     return node
   end
 end
 
-list_node_1 = ListNode.num_to_list_node(10000000001)
-list_node_2 = ListNode.num_to_list_node(465)
+# list_node_1 = ListNode.num_to_node(1000000000000000000000000000001)
+list_node_1 = ListNode.num_to_node(243)
+list_node_2 = ListNode.num_to_node(564)
 puts list_node_1.find_node_num
 puts list_node_2.find_node_num
 sum = list_node_1.find_node_num + list_node_2.find_node_num
 puts sum
-sum_node = ListNode.num_to_list_node(sum)
+sum_node = ListNode.num_to_node(sum)
 puts sum_node.find_node_num
